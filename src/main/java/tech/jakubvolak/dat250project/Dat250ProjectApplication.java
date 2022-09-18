e676d1f74d1a76c733111af25f6090216b9e2457
Jakub Volák <jakub.volak@seznam.cz>
2022-09-18 14:21:28 +0200

    Updated configuration for running project locally.

--- src/main/java/tech/jakubvolak/dat250project/Dat250ProjectApplication.java
+++ src/main/java/tech/jakubvolak/dat250project/Dat250ProjectApplication.java
@@ -1,30 +1,11 @@
 package tech.jakubvolak.dat250project;
 
-import com.zaxxer.hikari.HikariConfig;
-import com.zaxxer.hikari.HikariDataSource;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-
-import javax.sql.DataSource;
 
 @SpringBootApplication
 public class Dat250ProjectApplication {
-    @Value("${spring.datasource.url}")
-    private String dbUrl;
-
     public static void main(String[] args) {
         SpringApplication.run(Dat250ProjectApplication.class, args);
-    }
-    @Bean
-    public DataSource dataSource() {
-        if (dbUrl == null || dbUrl.isEmpty()) {
-            return new HikariDataSource();
-        } else {
-            HikariConfig config = new HikariConfig();
-            config.setJdbcUrl(dbUrl);
-            return new HikariDataSource(config);
-        }
     }
 }
