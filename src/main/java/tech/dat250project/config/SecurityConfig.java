ccd8d2315ba1d5bd5aa97882ee4f91191058a5ac
Jakub Volák <jakub.volak@seznam.cz>
2022-10-10 10:40:46 +0200

    Simplified security URL for swagger

--- src/main/java/tech/dat250project/config/SecurityConfig.java
+++ src/main/java/tech/dat250project/config/SecurityConfig.java
@@ -37,10 +37,15 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.csrf().disable().authorizeRequests().antMatchers("/login", "/register", "/register/device", "/vote/device",
-                        "/swagger-ui.html", "/swagger-ui/index.html", "/api-docs", "/swagger-ui/swagger-initializer.js",
-                        "/swagger-ui/swagger-ui-bundle.js", "/swagger-ui/swagger-ui-bundl", "/swagger-ui/index.css", "/swagger-ui/swagger-ui.css",
-                        "/swagger-ui/swagger-ui-standalone-preset.js", "/api-docs/swagger-config", "/swagger-ui/swagger-ui-stand", "/swagger-ui/favicon-32x32.png","/swagger-ui/favicon-16x16.png").permitAll()
+        http.csrf().disable().authorizeRequests().antMatchers("/login",
+                        "/register",
+                        "/register/device",
+                        "/vote/device",
+                        "/swagger-resources",
+                        "/swagger-resources/**",
+                        "/swagger-ui.html",
+                        "/api-docs/**",
+                        "/swagger-ui/**").permitAll()
                 .anyRequest().authenticated()
                 .and().logout().permitAll();
 
