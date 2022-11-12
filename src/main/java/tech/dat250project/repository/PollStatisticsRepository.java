5ab9a0345c355a07ba0e24f8b314eb81eb1f85c5
SrMateos <xavimateosm@gmail.com>
2022-11-12 18:32:49 +0100

    bugs

--- src/main/java/tech/dat250project/repository/PollStatisticsRepository.java
+++ src/main/java/tech/dat250project/repository/PollStatisticsRepository.java
@@ -1,16 +1,11 @@
 package tech.dat250project.repository;
 
-import java.util.List;
-
 import tech.dat250project.model.PollStatistics;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 
 
 public interface PollStatisticsRepository extends MongoRepository<PollStatistics, String> {
-
-    public PollStatistics findByFirstName(String firstName);
-    public List<PollStatistics> findByLastName(String lastName);
+    
     public PollStatistics findByPollId(Long pollId);
-
 }
